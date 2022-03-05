0ddcc8eb0f51bb707aebf1927b142f68b6e48900
mirz <mirz@localhost>
2022-03-05 15:24:48 -0500

    homework

---
+++ lsp-project/src/org/howard/edu/lsp/assignment5/implementation/IntegerSetException.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment5.implementation; 
+
+
+public class IntegerSetException extends Exception {
+ public IntegerSetException(String str)  
+ {  
+     // Call the constructor of parent exception
+     super(str); 
+ }  
+}
---
+++ lsp-project/src/org/howard/edu/lsp/assignment5/test/Driver.java
@@ -0,0 +1,41 @@
+package org.howard.edu.lsp.assignment5.test;
+
+//import java.util.*;
+
+import org.howard.edu.lsp.assignment5.implementation.IntegerSet;
+import org.howard.edu.lsp.assignment5.implementation.IntegerSetException;
+
+
+public class Driver
+{
+        public static void main(String[] args) {
+            IntegerSet set1 = new IntegerSet();
+            set1.add(1);
+            set1.add(2);
+            set1.add(3);
+                System.out.println("Value of Set1 is:" + set1);
+                
+                try {
+                    System.out.println("Smallest value in Set1 is:" + set1.smallest());
+                } catch(IntegerSetException ise) {
+                    System.out.println(ise);
+                }
+                
+                try {
+                    System.out.println("Largest value in Set1 is:" + set1.largest());
+                } catch(IntegerSetException ise) {
+                    System.out.println(ise);
+                }
+                
+                IntegerSet set2 = new IntegerSet();
+            set2.add(4);
+            set2.add(5);
+            
+            System.out.println("\nUnion of Set1 and Set2");
+            System.out.println("Value of Set1 is:" + set1);
+            System.out.println("Value of Set2 is:" + set2);
+            
+            set1.union(set2);
+            
+        }
+}
